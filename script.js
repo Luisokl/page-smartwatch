6e496c28f2857b399814bafd70be604188a2587d
Luis Fernando <100451426+Luisokl@users.noreply.github.com>
2023-10-06 03:08:27 -0300

    adicionando arquivos index.html, style, script.js e imagens do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $(".watch-slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: $("#arrow-prev"),
+        nextArrow: $("#arrow-next")
+    })
+})
